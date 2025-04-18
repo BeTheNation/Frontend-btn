6b9fdc998c8275cace74baf74241c4c2d9ae94d7
Danuardi <ruchapnara@gmail.com>
2025-04-18 10:09:33 +0700

    Enhance navbar and wallet button UI

--- components/ui/utils/connect-wallet-button.tsx
+++ components/ui/utils/connect-wallet-button.tsx
@@ -2,7 +2,7 @@
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useDemoMode } from "@/hooks/useDemoMode";
-import { useToast } from "@/components/ui/use-toast";
+import { useToast } from "@/components/ui/utils/use-toast";
 
 export function ConnectWalletButton() {
   const { isDemoMode } = useDemoMode();
@@ -54,7 +54,7 @@ export function ConnectWalletButton() {
                 return (
                   <button
                     type="button"
-                    className="btn-primary rounded-md px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-600"
+                    className="px-[20.82px] py-[13.01px] bg-gradient-to-br from-[#111214] to-[#22242a] rounded-[80.08px] shadow-[-9.609195709228516px_-9.609195709228516px_19.21839141845703px_0px_rgba(21,94,239,0.24)] shadow-[9.609195709228516px_9.609195709228516px_19.21839141845703px_0px_rgba(255,175,41,0.24)] outline outline-[2.40px] outline-[#155dee] inline-flex justify-center items-center gap-[10.41px] overflow-hidden"
                     onClick={() => {
                       handleBeforeConnect();
                       openConnectModal();
