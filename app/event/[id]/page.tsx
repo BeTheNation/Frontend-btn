9e0a09bf33300d4ba140b1573fde8b8beeabfdaa
ikhwanhsn <ikhwanulhusna111@gmail.com>
2025-06-19 07:13:22 +0700

    fix: build error, event page component not created yet

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,7 +1,12 @@
 import EventComponent from "./EventComponent";
 
 const EventPage = () => {
-  return <EventComponent />;
+  // return <EventComponent />;
+  return (
+    <main>
+      <h1>Event</h1>
+    </main>
+  );
 };
 
 export default EventPage;
